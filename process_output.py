3b50f2b94796fd161b281aec4fc4cfce405bfcb2
kevin-bruhwiler <kevin.bruhwiler@gmail.com>
2022-03-04 16:20:26 -0800

    added metrics for 'average recovery time' and 'total percentage agreement'

--- process_output.py
+++ process_output.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import auc
 from collections import defaultdict, deque
 
 
@@ -23,23 +24,36 @@ def get_block_at_time(time, adds):
 def analyze_agreement(pa):
 	drops = []
 	magnitudes = []
-	running_set = deque(3*[0], 5)
+	running_set = deque(40*[1], 40)
 	running_means = []
+	slopes = []
+	lines = []
 	last_time = float("-inf")
+	coords = None
+	max_val = float("-inf")
+	max_time = None
 	for i, (val, time) in enumerate(pa):
+		if val > max_val:
+			max_val = val
+			max_time = time
 		running_set.appendleft(val)
 		running_means.append(np.mean(running_set))
-		if val < running_means[-1] - 0.1 and time-last_time > 10000:
+		if val < running_means[-1] - 0.1 and time-last_time > 100000:
 			last_time = time
-			window = [val for val, _ in pa[i-10:i+10]]
+			window = [v for v, _ in pa[i-30:i+30]]
 			drops.append((time, val, max(window), min(window)))
 			magnitudes.append(max(window) - min(window))
+			if coords is not None:
+				lines.append([max_val, drops[-2][3], max_time, drops[-2][0]])
+				slopes.append((max_val-drops[-2][3])/(max_time-drops[-2][0]))
+			max_val = float("-inf")
+			coords = 1
 	
-	slopes = []
-	for i in range(1, len(drops)):
-		print(drops[i][2], drops[i-1][3], drops[i][0], drops[i-1][0])
-		slopes.append((drops[i][2]-drops[i-1][3])/(drops[i][0]-drops[i-1][0]))
-	return drops, running_means, magnitudes, slopes
+	#slopes = []
+	#for i in range(1, len(drops)):
+	#	print(drops[i][2], drops[i-1][3], drops[i][0], drops[i-1][0])
+	#	slopes.append((drops[i][2]-drops[i-1][3])/(drops[i][0]-drops[i-1][0]))
+	return drops, running_means, magnitudes, slopes, lines
 
 
 with open(".\\simulator\\src\\dist\\output\\output.json") as f:
@@ -68,10 +82,13 @@ for time in range(30000, max_timestamp, 100):
 	if max(sums, key=sums.get) is not None:
 		percentage_agreement.append([sums[max(sums, key=sums.get)]/len(node_vals), time])
 	
-events, means, drop_diffs, increase_rates = analyze_agreement(percentage_agreement)
-print(drop_diffs, increase_rates)
+events, means, drop_diffs, increase_rates, increase_vis = analyze_agreement(percentage_agreement)
+print(drop_diffs, increase_rates, auc([x[1] for x in percentage_agreement], [x[0] for x in percentage_agreement]))
 plt.plot([x[1] for x in percentage_agreement], [x[0] for x in percentage_agreement])
+plt.fill_between([x[1] for x in percentage_agreement], 0, [x[0] for x in percentage_agreement], alpha=0.2)
 plt.plot([x[0] for x in events], [x[1] for x in events], 'ro')
+for line in increase_vis:
+	plt.plot([line[2], line[3]], [line[0], line[1]], 'g')
 plt.ylabel("Precentage of Nodes with Majority Block")
 plt.xlabel("Timestamp")
 plt.title("Percentage Agreement")
